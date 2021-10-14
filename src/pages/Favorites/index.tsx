0d5823077ef44623f47546715f45ca21331e0325
brunogm2 <brunobgm002@gmail.com>
2021-10-14 16:38:05 -0300

    feat: favorites

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,10 +1,47 @@
 import React from "react";
-import { Container } from "./style";
+import { useDispatch, useSelector } from "react-redux";
+import FavoritesPokemons from "../../components/FavoritesPokemons";
+import Header from "../../components/Header";
+import { Container, Content, CardsList, TitlePage } from "./style";
+
 
 function Favorites() {
-    <Container>
 
-    </Container>
+    // const dispatch = useDispatch();
+    const favoriteList = useSelector((state:any) => state.favorite.data);
+
+    return(
+        <Container>
+            <Header/>
+            <Content>
+            
+            <TitlePage>
+                <h4>
+                    Favoritos
+                </h4>
+            </TitlePage> 
+                <CardsList>
+                {favoriteList != '' ?
+                   favoriteList.map((pokemon:any, index:any) => {
+                        return(<FavoritesPokemons
+                            key={index}
+                            id={pokemon.id}
+                            name={pokemon.name}
+                            image={pokemon.image}
+                            type={pokemon.type}
+                            />
+                        )
+                    })
+                    :  <h4 style={{marginLeft: "auto", marginRight: "auto", color: "#fff"}}>Não existe produtos favoritado!</h4> 
+                    
+                    }
+                </CardsList>
+                
+                
+            </Content>
+        </Container>
+    )
+
 }
 
 export default Favorites;
--- src/pages/Favorites/style.ts
+++ src/pages/Favorites/style.ts
@@ -1,5 +1,60 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+    background-image:  linear-gradient(15deg, rgba(39,42,50,1) 49%, rgba(66,75,99,1) 99%);
+    min-height: 100vh;
+`;
+
+export const Content = styled.div`
+    width: 90%;
+    height: 500px;
+    margin: 0 auto;
+    
+    >h3{
+        color: var(--color-letters-primary);
+        margin: 0px 0px 20px 50px;
+    }
+`; 
+
+export const CardsList = styled.div`
+    height: 90%;
+    width: 100%;
+    display: flex;
+    /* justify-content: center; */
+    /* align-items: center; */
+    flex-direction: row;
+    word-wrap: break-word;
+    flex-wrap: wrap;
+    overflow-y: scroll;
+    overflow-x: hidden;
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--color-inputs);
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: var(--color-scrollbar);
+    border-radius: 10px;
+
+  }
+
+`;  
+
+export const TitlePage = styled.div`
+    width: 130px;
+    padding-bottom: 8px;
+    border-bottom: 2px solid var(--color-button-reference);
+    display: flex;
+    flex-direction: row;
+    word-wrap: break-word;
+    margin: 0px 0px 20px 50px;
+    
+    >h4{
+        color: var(--color-letters-secondary);
+    }
 `;
