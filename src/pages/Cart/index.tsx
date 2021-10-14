71d5869be27ee96fbbc1c06c84d06289b80f1eec
brunogm2 <brunobgm002@gmail.com>
2021-10-14 16:39:04 -0300

    style: cart size

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, InputHTMLAttributes } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import { Container, Content, ContentData, NameDataType, ContentCard, DataSteam, FormSteam, Search,
          PaymentData, Card, Total, Checkout,
@@ -7,19 +7,38 @@ import { Container, Content, ContentData, NameDataType, ContentCard, DataSteam, 
         Action, DeleteCart
 
 } from "./style";
-import pokemon from "../../assets/pokemon.png"
 
 import Modal from '../../components/Modal';
 import Header from '../../components/Header';
 
 import { showMessage} from '../../store/ducks/layout';
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+    id: string
+    name: string;
+    image: string;
+    type: string; 
+   
+    deleteItem: (id:string, name:string, image:string, type:string) => void;
+}
 
-function Cart() {
+function Cart<Props>() {
 
+    const cart = useSelector((state:any) => state.cart.data)
     const dispatch = useDispatch();
+    
     const openModal = () => {
         dispatch(showMessage());
     };
+
+    const deleteItem = (id:string, name:string, image:string, type:string) => {
+        const data = ({id, name, image, type});
+
+        dispatch({
+            type: "CARRINHO_ITEM_DELETE",
+            payload: data
+        });
+    }
+
 
     return (
         <Container>
@@ -33,12 +52,12 @@ function Cart() {
                                 Dados da Steam
                             </h4>
                         </NameDataType> 
-                            <Search type="text" placeholder="Steam Hex"/>
+                            <Search type="text" name="hex" placeholder="Steam Hex"/>
                         <FormSteam>
-                            <Search type="text" placeholder="ID"/>
-                            <Search type="text" placeholder="Steam Guard" style={{marginLeft: "10px"}}/>
+                            <Search type="text" name="id" placeholder="ID"/>
+                            <Search type="text" name="guard" placeholder="Steam Guard" style={{marginLeft: "10px"}}/>
                         </FormSteam>
-                            <Search type="text" placeholder="Email"/>
+                            <Search type="text" name="email" placeholder="Email"/>
                     </DataSteam>
                     <PaymentData>
                         <NameDataType>
@@ -46,53 +65,71 @@ function Cart() {
                                 Dados de Pagamento
                             </h4>
                         </NameDataType> 
-                            <Search type="text" placeholder="Número do Cartão"/>
+                            <Search type="text" name="cartao" placeholder="Número do Cartão"/>
                         <FormSteam>
-                            <Search type="text" placeholder="Validade"/>
-                            <Search type="text" placeholder="CVV" style={{marginLeft: "10px"}}/>
+                            <Search type="text" name="validade" placeholder="Validade"/>
+                            <Search type="text" name="cavv" placeholder="CVV" style={{marginLeft: "10px"}}/>
                         </FormSteam>
                         <FormSteam>
-                            <Search type="text" placeholder="Nome do titular"/>
-                            <Search type="text" placeholder="CPF / CNPJ do titular" style={{marginLeft: "10px"}}/>
+                            <Search type="text" name="titular" placeholder="Nome do titular"/>
+                            <Search type="text" name="cpf" placeholder="CPF / CNPJ do titular" style={{marginLeft: "10px"}}/>
                         </FormSteam>
                     </PaymentData>
                 </ContentData>
                 <ContentCard>
+            
                     <Card>
-                        <h4>Meu Carrinho</h4>
-                        <CardList>
-                            <Img>
-                                <img src={pokemon} alt="imagem sslider1" style={{width: '100%', height:'100%'}} />     
-                            </Img>
-                            <Description>
-                                <h4>Eevee</h4>
-                                <p>Eletrecidade</p>
-                            </Description>
-                            <Value>
-                                <SymbolMoney>
-                                    <h6>R$:</h6>
-                                </SymbolMoney>
-                                <SubValue>
-                                    <h3>299,90</h3>
-                                </SubValue>
-                            </Value>
-                            <Action>
-                                <DeleteCart>
-                                    <h3>-</h3>
-                                </DeleteCart>
-                            </Action>
-                                        
-                        </CardList>
+                    <h4>Meu Carrinho</h4>
+                        
+                        {cart != '' ?
+                          cart.map((pokemon:any, index:any) => {
+                                return(
+                                    <CardList>
+                                        <Img>
+                                            <img src={pokemon.image} alt="imagem sslider1" style={{width: '100%', height:'100%'}} />     
+                                        </Img>
+                                        <Description>
+                                            <h4>{pokemon.name}</h4>
+                                            <p>{pokemon.type}</p>
+                                        </Description>
+                                        <Value>
+                                            <SymbolMoney>
+                                                <h6>R$:</h6>
+                                            </SymbolMoney>
+                                            <SubValue>
+                                                <h3>299,90</h3>
+                                            </SubValue>
+                                        </Value>
+                                        <Action>
+                                            <DeleteCart onClick={() => deleteItem(pokemon.id, pokemon.name, pokemon.type, pokemon.image)}>
+                                                <h3>-</h3>
+                                            </DeleteCart>
+                                        </Action>
+                                                    
+                                    </CardList>
+                                )
+                                })
+                                :
+                                <h4 style={{marginLeft: "auto", marginRight: "auto"}}>Sem produtos no carrinho!</h4> 
+                        }
+                        
                     </Card>
-                    <Total>
-                        <h5>Total:</h5>
-                        <h4>R$ 79,90</h4>
-                    </Total>
-                    <Checkout onClick={openModal}>
-                        <h3>
-                            Finalizar Compra
-                        </h3>
-                    </Checkout>
+                   
+                    {cart != '' ? 
+                        <>
+                            <Total>
+                                <h5>Total:</h5>
+                                <h4>R$ 299,90</h4>
+                            </Total>
+                            <Checkout onClick={openModal}>
+                                <h3>
+                                    Finalizar Compra
+                                </h3>
+                            </Checkout> 
+                        </>:
+                        null
+                    }
+                   
                 </ContentCard>
             </Content>
         </Container>  
--- src/pages/Cart/style.ts
+++ src/pages/Cart/style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { MdOutlineFavoriteBorder } from 'react-icons/md'
 import { RiShoppingCart2Line } from 'react-icons/ri'
 
-export const Container = styled.header`
+export const Container = styled.div`
     background-image:  linear-gradient(15deg, rgba(39,42,50,1) 49%, rgba(66,75,99,1) 99%);
     min-height: 100vh;
     display: flex;
@@ -21,30 +21,45 @@ export const Content = styled.div`
 
 export const ContentData = styled.div`
     width: 50%;
-    height: 100%;
+    height: 95%;
 `;
 
 export const ContentCard = styled(ContentData)`
-    /* background-color: red; */
     display: flex;
     flex-direction: column;
     align-items: flex-end;
-    /* justify-content: end; */
 `;
 
 export const Card = styled.div`
     width: 70%;
     height: 70%;
-    /* background-color: green; */
     margin-bottom: 10px;
-    border-radius: 10px;
+    border-radius: 10px 0px 0px 10px;
     border: 1px solid var(--color-border);
     padding: 20px;
+    overflow-y: scroll;
+    overflow-x: hidden;
+
     
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--color-inputs);
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: var(--color-scrollbar);
+    border-radius: 10px;
+  }
+
     >h4{
-        color: #FFFF;
+        color: var(--color-letters-secondary);
         margin-bottom: 10px;
     }
+    
 `;
 
 export const CardList = styled.div`
@@ -53,10 +68,9 @@ export const CardList = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
-    border-bottom: 1px solid #A9A9A9;
+    border-bottom: 1px solid var(--color-border);
     margin-bottom: 20px;
-    /* margin-right: 20px; */
-    /* background-color: red; */
+    
 `;
 
 export const Img = styled.div`
@@ -73,11 +87,11 @@ export const Description = styled.div`
     word-wrap: break-word;
 
     >h4{
-        color: #F9F9F9;
+        color: var(--color-letters-secondary);
     }
 
     >p{
-        color: #F9F9F9;
+        color: var(--color-letters-secondary);
         font-size: 12px;
     }
 `;
@@ -101,7 +115,7 @@ export const SymbolMoney = styled.div`
     margin-left: 10px;
 
     >h6{
-        color: #F9F9F9;
+        color: var(--color-letters-secondary);
         margin-top: 9px;
     }
 `;
@@ -115,7 +129,7 @@ export const SubValue = styled.div`
     margin: 5px;
 
     >h3{
-        color: #F9F9F9;
+        color: var(--color-letters-secondary);
     }
 
 `;
@@ -146,8 +160,9 @@ export const DeleteCart = styled.div`
     }
 `;
 
-export const Total = styled(Card)`
+export const Total = styled.div`
     height: 10%;
+    width: 70%;
     display:flex;
     flex-direction: row;
     justify-content: space-between;
@@ -155,44 +170,44 @@ export const Total = styled(Card)`
     border: none;
 
     >h4, h5{
-        color: #D2D2D2;
+        color: var(--color-letters-primary);
     }
 `;
 
-export const Checkout = styled(Card)`
+export const Checkout = styled.button`
     height: 10%;
+    width: 70%;
     background-color: greenyellow;
     display:flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
     border: none;
-    background-color: #FFCB06;
-    /* margin-left: 10px; */
+    background-color: var(--color-button-reference);
     -webkit-box-shadow: 1px 2px 23px 0px rgba(255,203,6,0.48);
     -moz-box-shadow: 1px 2px 23px 0px rgba(255,203,6,0.48);
     box-shadow: 1px 2px 23px 0px rgba(255,203,6,0.48);
     cursor: pointer;
 
     >h3{
-        color: #024EA3;
+        color: var(--color-letters-reference);;
     }
 `;
 
 export const NameDataType = styled.div`
     width: 190px;
     padding-bottom: 8px;
-    border-bottom: 2px solid #FFCB06;
+    border-bottom: 2px solid var(--color-button-reference);
     display: flex;
     flex-direction: row;
     word-wrap: break-word;
     
     >h4{
-        color: #FFFF;
+        color: var(--color-letters-secondary);
     }
 `;
 
-export const DataSteam = styled.div`
+export const DataSteam = styled.form`
     width: 100%;
     height: 45%;
 `;
@@ -205,19 +220,19 @@ export const FormSteam = styled.div`
 export const Search = styled.input`
     width: 100%;
     height: 40px;
-    background-color: #1C1C1F;
-    border: 1px solid #D2D2D2;
+    background-color: var(--color-inputs);
+    border: 1px solid var(--color-letters-primary);
     display: flex;
     justify-content: center;
     align-items: center;
     padding-left: 10px;
-    color: #D2D2D2;
+    color: var(--color-letters-primary);
     font-size: 15px;
     outline: 0;
     margin-top: 20px;
 
     ::placeholder {
-        color: #D2D2D2;
+        color: var(--color-letters-primary);
         font-size: 15px;
   }
 `;
@@ -225,5 +240,11 @@ export const Search = styled.input`
 export const PaymentData = styled(DataSteam)`
     margin-top: 18px;
     height: 55%;
-  
+
+`;
+
+export const Button = styled.button`
+    margin-top: 18px;
+    height: 55%;
+
 `;
