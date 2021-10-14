9ddbb739a249afba23740135d0a6504193c01de6
brunogm2 <brunobgm002@gmail.com>
2021-10-14 16:39:33 -0300

    feat: add favorite pokemon

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -4,8 +4,6 @@ import { CardList, Img, Description, Value, SubValue, SymbolMoney,
         Action, ButtonFavorite, ButtonCart, IconFavorite, IconCardList
 } from "./style"
 
-
-
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
     id: string
     name: string;
@@ -13,9 +11,10 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
     type: string; 
    
     addItemCart: (id:string, name:string, image:string, type:string,) => void;
+    addItemFavorite: (id:string, name:string, image:string, type:string,) => void;
 }
 
-const ProductsList: React.FC<Props> = ({ id, name, image, type, addItemCart }) => {
+const ProductsList: React.FC<Props> = ({ id, name, image, type, addItemCart, addItemFavorite }) => {
     
 return(
     <>
@@ -28,19 +27,20 @@ return(
             <h4>{name}</h4>
             <p>{type}</p>
         </Description>
-        <Value>
-            <SymbolMoney>
-                <h6>R$:</h6>
-            </SymbolMoney>
-            <SubValue>
-                <h3>299,90</h3>
-            </SubValue>
-        </Value>
+
         <Action >
-            <ButtonFavorite>
+            <ButtonFavorite onClick={() => addItemFavorite(id, name, image, type)}>
                 <IconFavorite/>
             </ButtonFavorite>
             <ButtonCart onClick={() => addItemCart(id, name, image, type)}>
+              
+                    <SymbolMoney>
+                        <h6>R$:</h6>
+                    </SymbolMoney>
+                    <SubValue>
+                        <h4>299,90</h4>
+                    </SubValue>
+             
                 <IconCardList/>
             </ButtonCart>
         </Action>
