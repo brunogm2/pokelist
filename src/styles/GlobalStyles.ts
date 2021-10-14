c383c1c08a63c929e845e707f6af389a6583ec27
brunogm2 <brunobgm002@gmail.com>
2021-10-14 16:34:16 -0300

    refactor: globalStyle

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -13,11 +13,13 @@ export default createGlobalStyle`
         --color-primary: #272A32;
         --color-secondary: #40444E;
         --color-tertiary: #000000;
-        --color-quaternary: #1C1C1F;
-        --color-border: #A9A9A9;
-        --color-letters-primary: #D2D2D2
-        --color-letters-secondary: #F9F9F9
-
+        --color-inputs: #1C1C1F;
+        --color-border: #A9A9A9;   
+        --color-letters-primary: #D2D2D2;
+        --color-letters-secondary: #F9F9F9;
+        --color-button-reference: #FFCB06;
+        --color-letters-reference: #024EA3;
+        --color-scrollbar: #40444E;
     }
 
 `;
